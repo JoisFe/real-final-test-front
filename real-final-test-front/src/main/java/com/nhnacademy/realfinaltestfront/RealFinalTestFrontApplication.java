1837badb6ef3e3a19380ee1b8cb0020e95cd7f2e
조재철 <rhsnqk@gmail.com>
2022-09-17 22:08:35 +0900

    Create a Spring Boot project with Maven

---
+++ real-final-test-front/src/main/java/com/nhnacademy/realfinaltestfront/RealFinalTestFrontApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.realfinaltestfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealFinalTestFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealFinalTestFrontApplication.class, args);
+	}
+
+}
